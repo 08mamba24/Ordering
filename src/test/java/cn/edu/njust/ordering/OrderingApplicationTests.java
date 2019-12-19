7461fde2d1588ddd8ec1f97f2a503ec64e713a9e
08mamba24 <864701928@qq.com>
2019-12-19 09:42:56 +0800

    a new ordering system was born!

---
+++ src/test/java/cn/edu/njust/ordering/OrderingApplicationTests.java
@@ -0,0 +1,13 @@
+package cn.edu.njust.ordering;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OrderingApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
